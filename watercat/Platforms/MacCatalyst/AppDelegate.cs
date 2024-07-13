87fcaabef32c0aa25e6e8e7358cb529b8ea00baa
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-07-13 17:19:46 +0200

    Setup
    Removed not so important files and modified .csproj to support .NET 8.0

--- watercat/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace watercat;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
