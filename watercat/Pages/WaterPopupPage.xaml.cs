3ca21734b29bd1e935988f55fa551197f0b6f6e2
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-07-17 15:02:06 +0200

    Added Popup page

---
+++ watercat/Pages/WaterPopupPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace watercat.Pages;
+
+public partial class WaterPopupPage : Popup
+{
+    public WaterPopupPage()
+    {
+        InitializeComponent();
+    }
+}
