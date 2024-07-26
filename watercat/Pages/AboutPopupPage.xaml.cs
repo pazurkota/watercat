7be192e4ee87a0a87b63a5ebdcf597efeb4f31ca
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-07-26 16:10:01 +0200

    Added websites into About popup

--- watercat/Pages/AboutPopupPage.xaml.cs
+++ watercat/Pages/AboutPopupPage.xaml.cs
@@ -4,8 +4,23 @@ namespace watercat.Pages;
 
 public partial class AboutPopupPage : Popup
 {
+    private const string GithubPageUri = "https://github.com/pazurkota/watercat";
+    private const string WebsiteUri = "https://pazurkota.me/";
+    
     public AboutPopupPage()
     {
         InitializeComponent();
+    }
+
+    private async void GithubRepoButton_OnClicked(object sender, EventArgs e)
+    {
+        Uri uri = new(GithubPageUri);
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void WebsiteButton_OnClicked(object sender, EventArgs e)
+    {
+        Uri uri = new(WebsiteUri);
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
     }
 }
