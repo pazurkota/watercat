c9095256ee43759ba039b810071d8c4c4e3169d9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-09-25 21:43:36 +0200

    Added code to buttons in UnitChoosePopup

--- watercat/Pages/Popups/UnitChoosePopup.xaml.cs
+++ watercat/Pages/Popups/UnitChoosePopup.xaml.cs
@@ -8,4 +8,19 @@ public partial class UnitChoosePopup : Popup
     {
         InitializeComponent();
     }
+
+    private void Button_SetMilliliters(object sender, EventArgs e)
+    {
+        Console.WriteLine("Set unit to milliliters!");
+    }
+
+    private void Button_SetOuncesUs(object sender, EventArgs e)
+    {
+        Console.WriteLine("Set unit to american ounces!");
+    }
+    
+    private void Button_SetOuncesUk(object sender, EventArgs e)
+    {
+        Console.WriteLine("Set unit to british ounces!");
+    }
 }
