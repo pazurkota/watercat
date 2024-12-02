b2291e9997bc433ac00c77c106fe1e89be0f6422
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-12-02 19:34:17 +0100

    Added auto increment for DailyWaterIntakeDb

--- watercat/Database/AppDbContext.cs
+++ watercat/Database/AppDbContext.cs
@@ -3,17 +3,13 @@ using watercat.Model;
 
 namespace watercat.Database;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
 	public DbSet<DailyWaterIntake> DailyWaterIntakes { get; set; }
-	
-	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-	{
-	}
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		base.OnModelCreating(modelBuilder);
-		modelBuilder.Entity<DailyWaterIntake>().HasKey(x => x.Id);
+		modelBuilder.Entity<DailyWaterIntake>().Property(x => x.Id).ValueGeneratedOnAdd();
 	}
 }
