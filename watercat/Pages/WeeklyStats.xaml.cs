a8ac29e374818a8b4162e02bcdb8fca2dc4524bd
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-12-27 17:28:22 +0100

    Added Weekly Stats page

---
+++ watercat/Pages/WeeklyStats.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace watercat.Pages;
+
+public partial class WeeklyStats : ContentPage
+{
+    public WeeklyStats()
+    {
+        InitializeComponent();
+    }
+}
